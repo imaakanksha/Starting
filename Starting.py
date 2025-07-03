d15b0a280cf0ef63f61cc8e3389ec9e3f5cb13e8
Akanksha <akankshaf7@gmail.com>
2025-07-03 22:56:58 +0530

    done

--- Starting.py
+++ Starting.py
@@ -1,1 +1,3 @@
 print("Starting the preparation from Today.i.e. 2nd July 2025")
+print("Well, again starting the practice again is just awesome and i would do this now everyday. on this day, one year back I had took the best decision of my life and now again on this day I am starting something awesome which i always wanted to and it would be also a huge success and the bestest decision of my life.")
+print("don")
